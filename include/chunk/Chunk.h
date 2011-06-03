44cc99e05f6971855f842b8f5444c93675cd98f6
Samuel L <gsam101@gmail.com>
2011-06-03 20:08:48 +0200

    Ajout d'une base de fichiers

---
+++ include/chunk/Chunk.h
@@ -0,0 +1,19 @@
+#ifndef CHUNK_CHUNK_H
+#define CHUNK_CHUNK_H
+#include <QtCore/QVector>
+namespace Chunk
+{
+    /**
+      * Contient une partie de la carte
+      * @brief Contient une partie de la carte
+      * @author Sam101
+      */
+    class Chunk
+    {
+        protected:
+
+        public:
+
+    };
+}
+#endif // CHUNK_H
