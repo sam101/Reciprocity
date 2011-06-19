021c5084d341e5b5a139bdf5f917b7b52ed44710
Samuel L <gsam101@gmail.com>
2011-06-19 13:10:53 +0200

    Ajout d'une première implémentation de getChunk

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -6,6 +6,10 @@ namespace World
       */
     World::World()
     {
+        _chunkTL.resize(1);
+        _chunkTR.resize(1);
+        _chunkBL.resize(1);
+        _chunkBR.resize(1);
 
     }
     /**
@@ -30,16 +34,41 @@ namespace World
         {
             if (y < 0)
             {
-
+                //On geneère les chunk qui n'existent pas.
+                if (abs(x) >= _chunkTL[0].size() || abs(y) >= _chunkTL.size())
+                {
+                    _generator->generate(x,y);
+                }
+                return _chunkTL[x][y];
             }
             else
             {
-
+                if (abs(x) >= _chunkBL[0].size() || abs(y) >= _chunkBL.size())
+                {
+                    _generator->generate(x,y);
+                }
+                return _chunkBL[x][y];
             }
         }
         else
         {
-
+            if (y < 0)
+            {
+                //On genère les chunk si il y'a besoin
+                if (x >= _chunkTR[0].size() || abs(y) >= _chunkTR.size())
+                {
+                    _generator->generate(x,y);
+                }
+                return _chunkTR[x][y];
+            }
+            else
+            {
+                if (x >= _chunkBR[0].size() || y >= _chunkBR.size())
+                {
+                    _generator->generate(x,y);
+                }
+                return _chunkBR[x][y];
+            }
         }
     }
 }
