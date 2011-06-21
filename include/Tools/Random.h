45cbd987645fff6c3b9b54e00b06d7ce3c11b0f6
Samuel L <gsam101@gmail.com>
2011-06-21 10:55:34 +0200

    Ajout du code de la classe Random

--- include/Tools/Random.h
+++ include/Tools/Random.h
@@ -2,9 +2,51 @@
 #define TOOLS_RANDOM_H
 namespace Tools
 {
+    /**
+     * Classe gérant la génération de nombres
+     * pseudo-aléatoires
+     * @brief Classe gérant la génération de nombres aléatoires
+     * @author Sam101
+     */
     class Random
     {
+    private:
+        /**
+         * Graine utilisée pour la génération des nombres aléatoires
+         */
+        static int _seed;
+        /**
+         * Nombre de nombres aléatoires déjà générés
+         */
+        static int _state;
+        /**
+         * Constructeur privé pour éviter de construire un objet de type Random
+         */
+         Random()
+         {
 
+         }
+    public:
+        /**
+         * Initialise le générateur de nombres pseudo-aléatoires
+         * @param seed Graine à utiliser
+         * @param Etat actuel du générateur
+         */
+        static void init(int seed, int state);
+        /**
+         * Renvoie un nombre pseudo-aléatoire entre min et max
+         * @param seed Minimum du nombre
+         * @param max Maximum du nombre
+        */
+         static int next(int min, int max);
+        /**
+          * Renvoie la seed actuelle
+          */
+        static int getSeed();
+        /**
+          * Renvoie le nombre de nombres aléatoires déjà générés
+          */
+        static int getState();
     };
 }
 #endif //TOOLS_RANDOM_H
--- src/Tools/Random.cpp
+++ src/Tools/Random.cpp
@@ -1,1 +1,51 @@
 #include <Tools/Random.h>
+#include <cstdlib>
+namespace Tools
+{
+    /**
+     * Graine utilisée pour la génération des nombres aléatoires
+     */
+    int Random::_seed = 0;
+    /**
+     * Nombre de nombres aléatoires déjà générés
+     */
+    int Random::_state = 0;
+    /**
+     * Initialise le générateur de nombres pseudo-aléatoires
+     * @param seed Graine à utiliser
+     * @param Etat actuel du générateur
+     */
+    void Random::init(int seed, int state)
+    {
+        _seed = seed;
+        _state = state;
+
+        for (int i = 0; i < _state; i++)
+        {
+            rand();
+        }
+    }
+    /**
+     * Renvoie un nombre pseudo-aléatoire entre min et max
+     * @param seed Minimum du nombre
+     * @param max Maximum du nombre
+     */
+     int Random::next(int min, int max)
+     {
+         return (rand() % (max - min)) + min;
+     }
+    /**
+     * Renvoie la seed actuelle
+     */
+    int Random::getSeed()
+    {
+        return _seed;
+    }
+    /**
+      * Renvoie le nombre de nombres aléatoires déjà générés
+      */
+    int Random::getState()
+    {
+        return _state;
+    }
+}
