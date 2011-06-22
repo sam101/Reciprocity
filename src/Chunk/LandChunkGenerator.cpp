9c09d59b7346fa20657c1c1df3039df8a305962a
Samuel L <gsam101@gmail.com>
2011-06-22 16:49:50 +0200

    Enlevé l'eau des bordures de LandChunkGenerator

--- src/Chunk/LandChunkGenerator.cpp
+++ src/Chunk/LandChunkGenerator.cpp
@@ -21,16 +21,7 @@ namespace Chunk
                 chunk->getTileAbs(j,i).setTile(Map::LOWLAND,Map::Tile::PASSABLE | Map::Tile::FARMING_CAPABLE);
             }
         }
-        /*
-         * On met de l'eau sur les bordures.
-         */
-        for (int i = 0; i < Config::Config::CHUNK_SIZE; i++)
-        {
-            chunk->getTileAbs(i,0).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
-            chunk->getTileAbs(0,i).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
-            chunk->getTileAbs(Config::Config::CHUNK_SIZE - 1,i).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
-            chunk->getTileAbs(i,Config::Config::CHUNK_SIZE - 1).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
-        }
+
         /*
          * On ajoute des montagnes parsemées
          */
