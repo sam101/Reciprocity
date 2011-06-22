ad628299ddc5cd9cd1a0e835ea8e838f27e6cb24
Samuel L <gsam101@gmail.com>
2011-06-22 15:37:06 +0200

    Amélioration du générateur de Land

--- src/Chunk/LandChunkGenerator.cpp
+++ src/Chunk/LandChunkGenerator.cpp
@@ -19,5 +19,15 @@ namespace Chunk
                 chunk->getTile(j,i).setTile(Map::LOWLAND,Map::Tile::PASSABLE | Map::Tile::FARMING_CAPABLE);
             }
         }
+        /*
+         * On met de l'eau sur les bordures.
+         */
+        for (int i = 0; i < Config::Config::CHUNK_SIZE; i++)
+        {
+            chunk->getTile(i,0).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
+            chunk->getTile(0,i).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
+            chunk->getTile(Config::Config::CHUNK_SIZE - 1,i).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
+            chunk->getTile(i,Config::Config::CHUNK_SIZE - 1).setTile(Map::SEA,Map::Tile::BOAT_PASSABLE);
+        }
     }
 }
