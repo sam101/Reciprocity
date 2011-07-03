18413221b867c6acb76024bf2cbbda438527e6a4
Samuel Lepetit <gsam101@gmail.com>
2011-07-03 20:31:17 +0200

    Ajout de l'implémentation de LoginFailedMessage

--- src/Network/LoginFailedMessage.cpp
+++ src/Network/LoginFailedMessage.cpp
@@ -1,1 +1,20 @@
 #include <Network/LoginFailedMessage.h>
+namespace Network
+{
+    /**
+      * Constructeur
+      */
+    LoginFailedMessage::LoginFailedMessage(qint32 why) :
+    AbstractMessage(LOGIN_FAILED),
+    _why(why)
+    {
+
+    }
+    /**
+      * Renvoie la raison pour laquelle le login a echoué
+      */
+    qint32 LoginFailedMessage::getWhy() const
+    {
+        return _why;
+    }
+}
