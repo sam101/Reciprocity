f560b21c65ed7b0c8df9ca28a96151f1e47a9265
Samuel Lepetit <gsam101@gmail.com>
2011-07-20 10:26:55 +0200

    Ajout de l'implémentation de sendLoginAlreadyExists

--- src/Server/MessageSender.cpp
+++ src/Server/MessageSender.cpp
@@ -1,4 +1,7 @@
 #include <Server/MessageSender.h>
+#include <Network/LoginFailedMessage.h>
+#include <QtCore/QByteArray>
+#include <QtCore/QDataStream>
 namespace Server
 {
     /**
@@ -7,9 +10,17 @@ namespace Server
       */
     void MessageSender::sendLoginAlreadyExists(QTcpSocket *socket, QString login)
     {
-        //TODO
-        Q_UNUSED(socket)
-        Q_UNUSED(login)
+        //On construit le message
+        Network::LoginFailedMessage m(Network::LoginFailedMessage::LOGIN_ALREADY_EXIST);
+        //On construit le byteArray dans lequel le mettre
+        QByteArray b;
+        QDataStream in(b);
+        in << (qint32)0;
+        in << m;
+        in.device()->seek(0);
+        in << (qint32)(b.size() - sizeof(qint32));
+        //On l'envoie
+        socket->write(b);
     }
     /**
       * Envoie un message au client comme quoi un login à réussi
