9d4c7e42b075f94b0b3835aaea4c57bc85a51d58
Samuel Lepetit <gsam101@gmail.com>
2011-07-22 19:42:06 +0200

    Implémentation de MessageInMessage

--- include/Network/MessageInMessage.h
+++ include/Network/MessageInMessage.h
@@ -1,5 +1,7 @@
 #ifndef NETWORK_MESSAGEINMESSAGE_H
 #define NETWORK_MESSAGEINMESSAGE_H
+#include <QtCore/QString>
+#include <Network/AbstractMessage.h>
 namespace Network
 {
     /**
@@ -7,9 +9,43 @@ namespace Network
       * @brief Stocke un message envoyé du Serveur au Client
       * @author Sam101
       */
-    class MessageInMessage
+    class MessageInMessage : public AbstractMessage
     {
-
+        public:
+            /**
+              * Magic number de MessageInMessage
+              */
+            static const qint32 MAGICNUMBER_MESSAGEINMESSAGE = 0x4209;
+        protected:
+            /**
+              * Expéditeur du message
+              */
+            QString _sender;
+            /**
+              * Contenu du message
+              */
+            QString _contents;
+       public:
+            /**
+              * Constructeur
+              */
+            MessageInMessage(QString sender = "", QString contents = "");
+            /**
+              * Renvoie l'expéditeur du message
+              */
+            QString getSender() const;
+            /**
+              * Renvoie le contenu du message
+              */
+            QString getContents() const;
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const MessageInMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, MessageInMessage &m);
     };
 }
 #endif //NETWORK_MESSAGEINMESSAGE_H
--- src/Network/MessageInMessage.cpp
+++ src/Network/MessageInMessage.cpp
@@ -1,5 +1,55 @@
 #include <Network/MessageInMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    MessageInMessage::MessageInMessage(QString sender, QString contents) :
+    AbstractMessage(Network::MESSAGE_IN),
+    _sender(sender),
+    _contents(contents)
+    {
 
+    }
+    /**
+      * Renvoie l'expéditeur du message
+      */
+    QString MessageInMessage::getSender() const
+    {
+        return _sender;
+    }
+    /**
+      * Renvoie le contenu du message
+      */
+    QString MessageInMessage::getContents() const
+    {
+        return _contents;
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const MessageInMessage &m)
+    {
+        //On envoie le MagicNumber
+        out << m.MAGICNUMBER_MESSAGEINMESSAGE;
+        //On envoie les données
+        out << m._sender;
+        out << m._contents;
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, MessageInMessage &m)
+    {
+        //On vérifie le MagicNumber
+        qint32 magicNumber;
+        in >> magicNumber;
+        if (magicNumber != m.MAGICNUMBER_MESSAGEINMESSAGE) return in;
+        //On recupère les données
+        in >> m._sender;
+        in >> m._contents;
+
+        return in;
+    }
 }
