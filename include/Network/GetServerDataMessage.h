5d12cbee0d4986a61881b6f059025df1f0c1e9f2
Samuel Lepetit <gsam101@gmail.com>
2011-07-24 12:16:16 +0200

    Première version de GetServerdataMessage fonctionelle

--- include/Network/GetServerDataMessage.h
+++ include/Network/GetServerDataMessage.h
@@ -8,9 +8,26 @@ namespace Network
       * @brief Message envoyé du client au serveur demandant les informations sur le serveur
       * @author Sam101
       */
-    class GetServerDataMessage
+    class GetServerDataMessage : public AbstractMessage
     {
         public:
+            /**
+              * MagicNumber de GetServerDataMessage
+              */
+            static const qint32 MAGICNUMBER_GET_SERVER_DATA = 0x4206;
+        public:
+            /**
+              * Constructeur
+              */
+            GetServerDataMessage();
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const GetServerDataMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, GetServerDataMessage &m);
     };
 }
 #endif //NETWORK_GETSERVERDATAMESSAGE_H
--- src/Network/GetServerDataMessage.cpp
+++ src/Network/GetServerDataMessage.cpp
@@ -1,5 +1,30 @@
 #include <Network/GetServerDataMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    GetServerDataMessage::GetServerDataMessage() :
+    AbstractMessage(Network::GET_SERVER_DATA)
+    {
 
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const GetServerDataMessage &m)
+    {
+        out << m.MAGICNUMBER_GET_SERVER_DATA;
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, GetServerDataMessage &m)
+    {
+        qint32 magicNumber;
+        in >> magicNumber;
+        //TODO: Vérification
+        return in;
+    }
 }
