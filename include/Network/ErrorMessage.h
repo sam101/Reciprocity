19d925d35d872be422f6a02652ef3c1832c83299
Samuel L <gsam101@gmail.com>
2011-08-01 13:57:13 +0200

    Ajout de l'implémentation de ErrorMessage

--- include/Network/ErrorMessage.h
+++ include/Network/ErrorMessage.h
@@ -8,9 +8,35 @@ namespace Network
       * @brief Message d'erreur envoyé du serveur au client
       * @author Sam101
       */
-    class ErrorMessage
+    class ErrorMessage : public AbstractMessage
     {
-
+        public:
+            /**
+              * MagicNumber de ErrorMessage
+              */
+            static const qint32 MAGICNUMBER_ERRORMESSAGE = -0x4201;
+        protected:
+            /**
+              * Intitulé du message d'erreur
+              */
+            QString _message;
+       public:
+            /**
+              * Constructeur
+              */
+            ErrorMessage(QString message = "");
+            /**
+              * Renvoie l'intitulé du message d'erreur
+              */
+            QString getMessage() const;
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const ErrorMessage &e);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, ErrorMessage &e);
     };
 }
 #endif //NETWORK_ERRORMESSAGE_H
--- src/Network/ErrorMessage.cpp
+++ src/Network/ErrorMessage.cpp
@@ -1,5 +1,46 @@
 #include <Network/ErrorMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    ErrorMessage::ErrorMessage(QString message) :
+    AbstractMessage(Network::ERROR),
+    _message(message)
+    {
 
+    }
+    /**
+      * Renvoie l'intitulé du message d'erreur
+      */
+    QString ErrorMessage::getMessage() const
+    {
+        return _message;
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const ErrorMessage &e)
+    {
+        //On envoie le MagicNumber
+        out << e.MAGICNUMBER_ERRORMESSAGE;
+        //on envoie les données
+        out << e._message;
+
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, ErrorMessage &e)
+    {
+        //On recupère le MagicNumber
+        qint32 magicNumber;
+        in >> magicNumber;
+        if (magicNumber != e.MAGICNUMBER_ERRORMESSAGE) return in;
+        //On recupère les données
+        in >> e._message;
+
+        return in;
+    }
 }
