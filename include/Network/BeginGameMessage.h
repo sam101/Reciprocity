e5ed23a59a2579e8e0863d442e604f6f1f7030df
Samuel L <gsam101@gmail.com>
2011-08-01 14:28:57 +0200

    Ajout de l'implémentation de BeginGameMessage

--- include/Network/BeginGameMessage.h
+++ include/Network/BeginGameMessage.h
@@ -1,5 +1,6 @@
 #ifndef NETWORK_BEGINGAMEMESSAGE_H
 #define NETWORK_BEGINGAMEMESSAGE_H
+#include <Network/AbstractMessage.h>
 namespace Network
 {
     /**
@@ -7,8 +8,25 @@ namespace Network
       * @brief Envoyé du client au serveur pour demander le début de la partie
       * @author Sam101
       */
-    class BeginGameMessage
+    class BeginGameMessage : public AbstractMessage
     {
+        public:
+            /**
+              * MagicNumber de BeginGameMessage
+              */
+            static const qint32 MAGICNUMBER_BEGINGAMEMESSAGE = 0x4210;
+            /**
+              * Constructeur
+              */
+            BeginGameMessage();
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const BeginGameMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, BeginGameMessage &m);
 
     };
 }
--- src/Network/BeginGameMessage.cpp
+++ src/Network/BeginGameMessage.cpp
@@ -1,5 +1,32 @@
 #include <Network/BeginGameMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    BeginGameMessage::BeginGameMessage() :
+    AbstractMessage(Network::BEGIN_GAME)
+    {
 
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const BeginGameMessage &m)
+    {
+        //On envoie le Magicnumber
+        out << m.MAGICNUMBER_BEGINGAMEMESSAGE;
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, BeginGameMessage &m)
+    {
+        Q_UNUSED(m)
+        //TODO
+        qint32 magicNumber;
+        in >> magicNumber;
+        return in;
+    }
 }
