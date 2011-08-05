47a86a24b81dde4570176ae8b2985221b9d52822
Samuel L <gsam101@gmail.com>
2011-08-05 12:04:45 +0200

    Ajout d'une première implémentation de DesertLandChunkGenerator

--- src/Chunk/DesertLandChunkGenerator.cpp
+++ src/Chunk/DesertLandChunkGenerator.cpp
@@ -1,4 +1,6 @@
 #include <Chunk/DesertLandChunkGenerator.h>
+#include <Tools/Random.h>
+using namespace Tools;
 namespace Chunk
 {
     /**
@@ -7,7 +9,31 @@ namespace Chunk
       */
     void DesertLandChunkGenerator::generate(Chunk *chunk)
     {
-
+        /*
+         * On genère de l'eau sur tout le chunk tout d'abord.
+         */
+        for (int i = 0; i  < Config::Config::CHUNK_SIZE; i++)
+        {
+            for (int j = 0; j < Config::Config::CHUNK_SIZE; j++)
+            {
+                chunk->getTileAbs(j,i).setAsSea();
+            }
+        }
+        /*
+          * On genère une plage géante
+          */
+        int xDeb, yDeb, xFin, yFin;
+        xDeb = Random::next(0,2);
+        yDeb = Random::next(0,2);
+        xFin = Random::next(Config::Config::CHUNK_SIZE - 2,Config::Config::CHUNK_SIZE);
+        yFin = Random::next(Config::Config::CHUNK_SIZE - 2,Config::Config::CHUNK_SIZE);
+        for (int i = xDeb; i <= xFin; i++)
+        {
+            for (int j = yDeb; j <= yFin; j++)
+            {
+                chunk->getTileAbs(i,j).setAsBeach();
+            }
+        }
     }
 
 }
