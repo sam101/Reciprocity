58f9ebd933dacc2646d5060dcf6b05a6964c0162
Samuel L <gsam101@gmail.com>
2011-08-08 19:43:15 +0200

    Ajout de l'implémentation de MoveUnitAcceptedMessage

--- include/Network/MoveUnitAcceptedMessage.h
+++ include/Network/MoveUnitAcceptedMessage.h
@@ -3,13 +3,51 @@
 #include <Network/AbstractMessage.h>
 namespace Network
 {
-    class MoveUnitAcceptedMessage
+    class MoveUnitAcceptedMessage : public AbstractMessage
     {
         public:
             /**
               * MagicNumber de MoveUnitAcceptedMessage
               */
             static const qint32 MAGICNUMBER_MOVEUNIT_ACCEPTED = 0x4216;
+        protected:
+            /**
+              * Id de l'entité qui a été déplacée
+              */
+            qint32 _id;
+            /**
+              * Nouvelle position X de l'entité
+              */
+            qint32 _x;
+            /**
+              * Nouvelle position Y de l'entité
+              */
+            qint32 _y;
+        public:
+            /**
+              * Constructeur
+              */
+            MoveUnitAcceptedMessage(qint32 id = -1, qint32 x = 0, qint32 y = 0);
+            /**
+              * Renvoie l'id de l'entité déplacée
+              */
+            qint32 getId() const;
+            /**
+              * Renvoie la position X de l'entité
+              */
+            qint32 getX() const;
+            /**
+              * Renvoie la position Y de l'entité
+              */
+            qint32 getY() const;
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const MoveUnitAcceptedMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, MoveUnitAcceptedMessage &m);
     };
 }
 #endif //NETWORK_MOVEUNITACCEPTED_H
--- src/Network/MoveUnitAcceptedMessage.cpp
+++ src/Network/MoveUnitAcceptedMessage.cpp
@@ -1,5 +1,69 @@
 #include <Network/MoveUnitAcceptedMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    MoveUnitAcceptedMessage::MoveUnitAcceptedMessage(qint32 id, qint32 x, qint32 y) :
+    AbstractMessage(Network::MOVEUNIT_ACCEPTED),
+    _id(id),
+    _x(x),
+    _y(y)
+    {
 
+    }
+    /**
+      * Renvoie l'id de l'entité déplacée
+      */
+    qint32 MoveUnitAcceptedMessage::getId() const
+    {
+        return _id;
+    }
+    /**
+      * Renvoie la position X de l'entité
+      */
+    qint32 MoveUnitAcceptedMessage::getX() const
+    {
+        return _x;
+    }
+    /**
+      * Renvoie la position Y de l'entité
+      */
+    qint32 MoveUnitAcceptedMessage::getY() const
+    {
+        return _y;
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const MoveUnitAcceptedMessage &m)
+    {
+        //On envoie le magicNumber
+        out << m.MAGICNUMBER_MOVEUNIT_ACCEPTED;
+        //On envoie les données
+        out << m._id;
+        out << m._x;
+        out << m._y;
+
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, MoveUnitAcceptedMessage &m)
+    {
+        //On vérifie le magicNumber
+        qint32 magicNumber;
+        in >> magicNumber;
+        if (magicNumber != m.MAGICNUMBER_MOVEUNIT_ACCEPTED)
+        {
+            return in;
+        }
+        //On recupère les données
+        in >> m._id;
+        in >> m._x;
+        in >> m._y;
+
+        return in;
+    }
 }
