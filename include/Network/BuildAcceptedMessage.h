6051bf85a6f38773f67c1d6544a43767a53369fe
Samuel L <gsam101@gmail.com>
2011-08-11 22:21:26 +0200

    Ajout de l'implémentation de BuildAcceptedMessage

--- include/Network/BuildAcceptedMessage.h
+++ include/Network/BuildAcceptedMessage.h
@@ -8,13 +8,25 @@ namespace Network
       * @brief Signale qu'une construction a été acceptée
       * @author Sam101
       */
-    class BuildAcceptedMessage
+    class BuildAcceptedMessage : public AbstractMessage
     {
         public:
             /**
               * MagicNumber de BuildAcceptedMessage
               */
             static const qint32 MAGICNUMBER_BUILD_ACCEPTED = 0x4218;
+            /**
+              * Constructeur
+              */
+            BuildAcceptedMessage();
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const BuildAcceptedMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, BuildAcceptedMessage &m);
     };
 }
 #endif //NETWORK_BUILDACCEPTEDMESSAGE_H
--- src/Network/BuildAcceptedMessage.cpp
+++ src/Network/BuildAcceptedMessage.cpp
@@ -1,5 +1,34 @@
 #include <Network/BuildAcceptedMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    BuildAcceptedMessage::BuildAcceptedMessage() :
+    AbstractMessage(Network::BUILD_ACCEPTED)
+    {
 
+    }
+
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const BuildAcceptedMessage &m)
+    {
+        //On envoie le magicNumber
+        out << m.MAGICNUMBER_BUILD_ACCEPTED;
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, BuildAcceptedMessage &m)
+    {
+        Q_UNUSED(m)
+        //On recupère le magicNumber
+        qint32 magicNumber;
+        in >> magicNumber;
+        //TODO: Vérification
+        return in;
+    }
 }
