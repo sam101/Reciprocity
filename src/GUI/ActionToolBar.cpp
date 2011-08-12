9b545d976951a7805daf682376b9c2b1482fc957
Samuel L <gsam101@gmail.com>
2011-08-12 11:14:06 +0200

    Ajouts dans ActionToolBar

--- src/GUI/ActionToolBar.cpp
+++ src/GUI/ActionToolBar.cpp
@@ -8,16 +8,20 @@ namespace GUI
       */
     ActionToolBar::ActionToolBar()
     {
+        setWindowTitle(tr("Barre d'actions"));
+        setFloatable(false);
         //On construit le bouton de fin de tour
         QPushButton *endTurn = new QPushButton(tr("Fin du tour"));
         addWidget(endTurn);
+        addSeparator();
         //On construit le bouton de déplacement
         QPushButton *move = new QPushButton(tr("Déplacement"));
         addWidget(move);
+        addSeparator();
         //On construit le bouton pour construire
         QPushButton *build = new QPushButton(tr("Construire"));
         addWidget(build);
-
+        addSeparator();
 
     }
 }
