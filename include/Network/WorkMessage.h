087735d8ae113ce2f6c21e3b4de0c8c9218efac3
Samuel L <gsam101@gmail.com>
2011-09-13 15:47:43 +0200

    Ajouts dans WorkMessage

--- include/Network/WorkMessage.h
+++ include/Network/WorkMessage.h
@@ -3,9 +3,23 @@
 #include <Network/AbstractMessage.h>
 namespace Network
 {
-    class WorkMessage
+    class WorkMessage : public AbstractMessage
     {
-
+        public:
+            /**
+              * MagicNumber de WorkMessage
+              */
+            static const qint32 MAGICNUMBER_WORK = 0x4219;
+        protected:
+            /**
+              * Entité qui doit travailler
+              */
+            qint32 _entity;
+        public:
+            /**
+              * Constructeur
+              */
+            WorkMessage(qint32 entity = -1);
     };
 }
 #endif //NETWORK_WORKMESSAGE_H
