f0c5ad810c5df46310bcc7143d35b021a43b6164
Samuel L <gsam101@gmail.com>
2011-09-16 13:44:23 +0200

    Ajouts dans le header de BuildingInfoWidget

--- include/GUI/BuildingInfoWidget.h
+++ include/GUI/BuildingInfoWidget.h
@@ -1,5 +1,9 @@
 #ifndef GUI_BUILDINGINFOWIDGET_H
 #define GUI_BUILDINGINFOWIDGET_H
+#include <Map/Building.h>
+#include <QtGui/QLabel>
+#include <QtGui/QPixmap>
+#include <QtCore/QVector>
 #include <QtGui/QWidget>
 namespace GUI
 {
@@ -11,6 +15,23 @@ namespace GUI
     class BuildingInfoWidget : public QWidget
     {
         Q_OBJECT
+        protected:
+            /**
+              * Batiment actuel
+              */
+            Map::Building _building;
+            /**
+              * Label contenant l'image du batiment
+              */
+            QLabel *_imageLabel;
+            /**
+              * Label contenant les infos sur le batiment
+              */
+            QLabel *_infoLabel;
+            /**
+              * Tableau contenant les pixmap des batiments
+              */
+            QVector<QPixmap> _buildings;
     };
 }
 #endif //GUI_BUILDINGINFOWIDGET_H
