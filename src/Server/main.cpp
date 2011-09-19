d9be5e10e7026c33282d7c91c6fa9cda98724b7e
Samuel L <gsam101@gmail.com>
2011-09-19 09:10:59 +0200

    Ajout du nom/port du Serveur dans le main de Server

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
 {
     //On construit l'objet application.
     QCoreApplication a(argc,argv);
+    //On déclare les variables pour le port/nom du serveur
+    qint32 serverPort = Config::Config::SERVER_PORT;
+    QString name = QObject::tr("Yet another Reciprocity server");
     //On initialise Random
     Random::init(time(NULL));
     //On déclare qu'on utilise UTF-8.
@@ -17,7 +20,7 @@ int main(int argc, char *argv[])
     qDebug() << "Serveur de Reciprocity - Version de dev (git)";
     //On construit un objet serveur.
     Server::Server *server = new Server::Server;
-    server->init();
+    server->init(serverPort,name);
     //On execute l'application
     return a.exec();
 }
