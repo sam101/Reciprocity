b8e45a634f37718b6a4d3cff0a29d64f5c73f14b
Samuel L <gsam101@gmail.com>
2011-09-19 09:15:01 +0200

    Ajout de la gestion des arguments de la ligne de commandes dans Client

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <Server/Server.h>
 #include <Tools/Random.h>
+#include <QtCore/QStringList>
 using namespace Tools;
 int main(int argc, char *argv[])
 {
@@ -13,6 +14,21 @@ int main(int argc, char *argv[])
     QString name = QObject::tr("Yet another Reciprocity server");
     //On initialise Random
     Random::init(time(NULL));
+    //On parse les arguments passés à la ligne de commande
+    QStringList args = a.arguments();
+    for (int i = 0; i < args.size(); i++)
+    {
+        //On gère le port du serveur
+        if (args[i] == "-port" && i != (args.size() - 1))
+        {
+            serverPort = args.at(i + 1).toInt();
+        }
+        //On gère le nom du serveur
+        else if (args[i] == "-name" && i != (args.size() - 1))
+        {
+            name = args[i + 1];
+        }
+    }
     //On déclare qu'on utilise UTF-8.
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
