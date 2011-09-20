dc8487555790c1ac3cc81c088a1170267e97d5af
Samuel L <gsam101@gmail.com>
2011-09-20 15:52:22 +0200

    Ajouts dans HelloMessage

--- include/Network/HelloMessage.h
+++ include/Network/HelloMessage.h
@@ -1,6 +1,6 @@
 #ifndef NETWORK_HELLOMESSAGE_H
 #define NETWORK_HELLOMESSAGE_H
-#include <Network/HelloMessage.h>
+#include <Network/AbstractMessage.h>
 namespace Network
 {
     /**
@@ -10,7 +10,25 @@ namespace Network
       */
     class HelloMessage
     {
-
+        public:
+            /**
+              * MagicNumber de HelloMessage
+              */
+            static const qint32 MAGICNUMBER_HELLOMESSAGE = 0x4200;
+        protected:
+            /**
+              * Nom du serveur
+              */
+            QString _name;
+            /**
+              * Version du protocole
+              */
+            qint32 _version;
+        public:
+            /**
+              * Constructeur
+              */
+            HelloMessage(QString name, qint32 version);
     };
 }
 #endif //NETWORK_HELLOMESSAGE_H
--- src/Network/HelloMessage.cpp
+++ src/Network/HelloMessage.cpp
@@ -1,5 +1,14 @@
 #include <Network/HelloMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    HelloMessage::HelloMessage(QString name, qint32 version) :
+    AbstractMessage(Network::HELLO),
+    _name(name),
+    _version(version)
+    {
 
+    }
 }
