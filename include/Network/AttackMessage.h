503bede76f6b200402a9bae9a7dedce203ea5f6e
Samuel L <gsam101@gmail.com>
2011-09-23 11:45:18 +0200

    Ajouts dans AttackMessage

--- include/Network/AttackMessage.h
+++ include/Network/AttackMessage.h
@@ -1,5 +1,6 @@
 #ifndef NETWORK_ATTACKMESSAGE_H
 #define NETWORK_ATTACKMESSAGE_H
+#include <Network/AbstractMessage.h>
 namespace Network
 {
     /**
@@ -9,7 +10,41 @@ namespace Network
       */
     class AttackMessage
     {
-
+        public:
+            /**
+              * Magic Number de AttackMessage
+              */
+            static const qint32 MAGICNUMBER_ATTACK = 0x4224;
+        protected:
+            /**
+              * Id de l'entité qui doit attaquer
+              */
+            qint32 _entityId;
+            /**
+              * Position X à attaquer
+              */
+            qint32 _x;
+            /**
+              * Position Y à attaquer
+              */
+            qint32 _y;
+        public:
+            /**
+              * Constructeur
+              */
+            AttackMessage(qint32 entityId = -1, qint32 x = 0, qint32 y = 0);
+            /**
+              * Renvoie l'id de l'entité
+              */
+            qint32 getId() const;
+            /**
+              * Renvoie la position X à attaquer
+              */
+            qint32 getX() const;
+            /**
+              * Renvoie la position Y à attaquer
+              */
+            qint32 getY() const;
     };
 }
 #endif //NETWORK_ATTACKMESSAGE_H
--- src/Network/AttackMessage.cpp
+++ src/Network/AttackMessage.cpp
@@ -1,5 +1,36 @@
 #include <Network/AttackMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    AttackMessage::AttackMessage(qint32 entityId, qint32 x, qint32 y) :
+    AbstractMessage(Network::ATTACK),
+    _entityId(entityId),
+    _x(x),
+    _y(y)
+    {
 
+    }
+    /**
+      * Renvoie l'id de l'entité
+      */
+    qint32 AttackMessage::getId() const
+    {
+        return _id;
+    }
+    /**
+      * Renvoie la position X à attaquer
+      */
+    qint32 AttackMessage::getX() const
+    {
+        return _x;
+    }
+    /**
+      * Renvoie la position Y à attaquer
+      */
+    qint32 AttackMessage::getY() const
+    {
+        return _y;
+    }
 }
