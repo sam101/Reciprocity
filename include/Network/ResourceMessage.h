cacd135a6f74cc7e50b3cff2fa0ebe5ff30265b8
Samuel L <gsam101@gmail.com>
2011-09-26 14:01:31 +0200

    Ajouts dans ResourceMessage

--- include/Network/ResourceMessage.h
+++ include/Network/ResourceMessage.h
@@ -10,9 +10,42 @@ namespace Network
       * @brief Gère un déplacement de ressources
       * @author Sam101
       */
-    class ResourceMessage
+    class ResourceMessage : public AbstractMessage
     {
+        public:
+            /**
+              * MagicNumber de ResourceMessage
+              */
+            static const qint32 MAGICNUMBER_RESOURCE = 0x4221;
+            /**
+              * Types de déplacement possibles
+              */
+            enum MoveType
+            {
+                NONE = -1,
+                ENTITY_TILE = 0,
+                TILE_ENTITY = 1,
+                ENTITY_ENTITY = 2
+            };
 
+        protected:
+            /**
+              * Type de déplacement
+              */
+            MoveType _type;
+            /**
+              * Id de l'entité source
+              */
+            qint32 _entityId;
+            /**
+              * Id de l'entité destination (si besoin)
+              */
+            qint32 _entityIdDest;
+        public:
+            /**
+              * Constructeur
+              */
+            ResourceMessage(MoveType type = NONE, qint32 entityId = -1, qint32 entityIdDest = -1);
     };
 }
 #endif //NETWORK_RESOURCEMESSAGE_H
--- src/Network/ResourceMessage.cpp
+++ src/Network/ResourceMessage.cpp
@@ -1,5 +1,15 @@
 #include <Network/ResourceMessage.h>
 namespace Network
 {
+    /**
+      * Constructeur
+      */
+    ResourceMessage::ResourceMessage(MoveType type, qint32 entityId, qint32 entityIdDest) :
+    AbstractMessage(Network::RESOURCE),
+    _type(type),
+    _entityId(entityId),
+    _entityIdDest(entityIdDest)
+    {
 
+    }
 }
