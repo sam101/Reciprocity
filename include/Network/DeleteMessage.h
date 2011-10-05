25fde2a272cfb3f17742ba55b3c684e5c72cf4f3
Samuel L <gsam101@gmail.com>
2011-10-05 19:53:59 +0200

    Ajouts dans DeleteMessage

--- include/Network/DeleteMessage.h
+++ include/Network/DeleteMessage.h
@@ -47,6 +47,26 @@ namespace Network
               * Constructeur
               */
             DeleteMessage(DeleteType type = NONE, qint32 x = 0, qint32 y = 0, qint32 entityId = -1);
+            /**
+              * Renvoie la position X
+              */
+            qint32 getX() const;
+            /**
+              * Renvoie la position Y
+              */
+            qint32 getY() const;
+            /**
+              * Renvoie l'id de l'entité à détruire
+              */
+            qint32 getEntityId() const;
+            /**
+              * Envoie le message dans un QDataStream
+              */
+            friend QDataStream& operator<<(QDataStream &out, const DeleteMessage &m);
+            /**
+              * Recupère le message d'un QDataStream
+              */
+            friend QDataStream& operator>>(QDataStream &in, DeleteMessage &m);
     };
 }
 #endif //NETWORK_DELETEMESSAGE_H
--- src/Network/DeleteMessage.cpp
+++ src/Network/DeleteMessage.cpp
@@ -13,4 +13,49 @@ namespace Network
     {
 
     }
+    /**
+      * Renvoie la position X
+      */
+    qint32 DeleteMessage::getX() const
+    {
+        return _x;
+    }
+    /**
+      * Renvoie la position Y
+      */
+    qint32 DeleteMessage::getY() const
+    {
+        return _y;
+    }
+    /**
+      * Renvoie l'id de l'entité à détruire
+      */
+    qint32 DeleteMessage::getEntityId() const
+    {
+        return _entityId;
+    }
+    /**
+      * Envoie le message dans un QDataStream
+      */
+    QDataStream& operator<<(QDataStream &out, const DeleteMessage &m)
+    {
+        out << m.MAGICNUMBER_DELETE;
+        out << (qint32)m._type;
+        out << m._x;
+        out << m._y;
+        out << m._entityId;
+
+        return out;
+    }
+    /**
+      * Recupère le message d'un QDataStream
+      */
+    QDataStream& operator>>(QDataStream &in, DeleteMessage &m)
+    {
+        qint32 magicNumber;
+        in >> magicNumber;
+        in >> (qint32&)m._type;
+
+        return in;
+    }
 }
